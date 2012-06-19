94f20342811a1ea9de3efc6ceab287ba9edb287c
Szymon Wr?blewski <bluex0@gmail.com>
2012-06-19 13:51:30 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PygameNetwork',
+    version='0.1dev',
+    packages=['pygame_network',],
+    license=open('LICENSE.txt').readline().strip(),
+    long_description=open('README.txt').read(),
+)
